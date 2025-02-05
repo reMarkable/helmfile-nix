4acd682236538fd004ff268eb10a9195fc324fb9
Marcus Ramberg <marcus@means.no>
2025-02-05 08:32:45 +0100

    feat: setup devenv shell

--- flake.nix
+++ flake.nix
@@ -1,27 +1,55 @@
 {
   description = "helmfile-nix";
 
+  inputs.devenv.url = "github:cachix/devenv";
+  inputs.devenv.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.gomod2nix.url = "github:nix-community/gomod2nix";
   inputs.gomod2nix.inputs.nixpkgs.follows = "nixpkgs";
   inputs.gomod2nix.inputs.flake-utils.follows = "flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils, gomod2nix }:
-    (flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      devenv,
+      flake-utils,
+      gomod2nix,
+      nixpkgs,
+      self,
+    }@inputs:
+    (flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
         # The current default sdk for macOS fails to compile go projects, so we use a newer one for now.
         # This has no effect on other platforms.
-        callPackage =
-          pkgs.darwin.apple_sdk_11_0.callPackage or pkgs.callPackage;
-      in {
+        callPackage = pkgs.darwin.apple_sdk_11_0.callPackage or pkgs.callPackage;
+      in
+      {
         packages.default = callPackage ./. {
           inherit (gomod2nix.legacyPackages.${system}) buildGoApplication;
         };
-        devShells.default = callPackage ./shell.nix {
-          inherit (gomod2nix.legacyPackages.${system}) mkGoEnv gomod2nix;
+        devShells.default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            (
+              { pkgs, ... }:
+              let
+                goEnv = gomod2nix.legacyPackages.${system}.mkGoEnv { pwd = ./.; };
+              in
+              {
+                packages = with pkgs; [
+                  goEnv
+                  gomod2nix.legacyPackages.${system}.gomod2nix
+                  docker
+                  helmfile
+                  kubernetes-helm
+                ];
+              }
+            )
+          ];
         };
-      }));
+      }
+    ));
 }

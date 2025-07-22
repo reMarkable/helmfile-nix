8dc4028b383556bac3d66505d95e541b89b79b66
Marcus Ramberg <marcus@means.no>
2025-07-22 13:36:20 +0200

    test: setup integration test for new nixChart feature

--- testData/nixChart/chart.nix
+++ testData/nixChart/chart.nix
@@ -1,45 +1,47 @@
 { val, ... }:
+let
+  containerPort = val.containerPort or 80;
+  replicas = val.replicas or 3;
+  version = val.version or "1.14.2";
+in
 [
   {
-    name = "test-chart";
-    templates = [
-      {
-        apiVersion = "karpenter.k8s.aws/v1";
-        kind = "EC2NodeClass";
+    apiVersion = "apps/v1";
+    kind = "Deployment";
+    metadata = {
+      name = "nginx-deployment";
+      namespace = val.namespace;
+      labels = {
+        app = "nginx";
+      };
+    };
+    spec = {
+      inherit replicas;
+      selector = {
+        matchLabels = {
+          app = "nginx";
+        };
+      };
+      template = {
         metadata = {
-          name = "default";
-          namespace = val.namespace;
+          labels = {
+            app = "nginx";
+          };
         };
         spec = {
-          amiSelectorTerms = [ { alias = "al2023@latest"; } ];
-          userData = ''
-                MIME-Version: 1.0
-                Content-Type: multipart/mixed; boundary="//"
-
-                --//
-                Content-Type: application/node.eks.aws
-
-            # Example custom nodeconfig which mounts individual drives on an instance
-                apiVersion: node.eks.aws/v1alpha1
-                kind: NodeConfig
-                spec:
-                  containerd:
-                    config: |
-                      [plugins."io.containerd.grpc.v1.cri".containerd]
-                      discard_unpacked_layers = false
-                  instance:
-                    localStorage:
-                      strategy: Raid0
-                --//
-          '';
-          kubelet.maxPods = 110;
-          metadataOptions.httpPutResponseHopLimit = 2;
-          role = val.karpenter_instance_profile_role;
-          securityGroupSelectorTerms = [ { "tags.karpenter.sh/discovery" = val.cluster_name; } ];
-          subnetSelectorTerms = [ { tags."karpenter.sh/discovery" = val.cluster_name; } ];
-          tags."karpenter.sh/discovery" = val.cluster_name;
+          containers = [
+            {
+              name = "nginx";
+              image = "nginx:${version}";
+              ports = [
+                {
+                  inherit containerPort;
+                }
+              ];
+            }
+          ];
         };
-      }
-    ];
+      };
+    };
   }
 ]
